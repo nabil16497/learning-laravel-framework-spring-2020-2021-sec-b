3554f5e25c2cf449479ab7c6c0d2884022325196
nabil16497 <nabil16497@gmail.com>
2021-03-01 10:40:36 +0600

    march1

--- test/routes/web.php
+++ test/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function(){
+    return '<h1>Hello World</h1>';
+});
+
+Route::get('/about', function(){
+    return view('welcome');
+});
+
+Route::get('/user/{id}', function($id){
+    return 'This is '.$id;
+});
+
